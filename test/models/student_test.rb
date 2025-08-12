213add341c45546a13751ce50ee2684fc845ee5e
Kyle <kdevos827@gmail.com>
2025-08-12 22:20:32 +0200

    Created First test checking student count method. Modified test helper - needed as 'fixtures' were being created inflating the number of rows in the tables

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+
+  # Test that student count works correctly
+  test 'check get_students_count returns correct counts' do
+    puts "Before test, Student.count: #{Student.count}"
+    
+    grade = Grade.create!(grade_value: 8);
+    Student.create!(grade: grade, full_name: "Nancy Jinkings", absent_days: 2)
+    Student.create!(grade: grade, full_name: "Peter Donald", absent_days: 5)
+    Student.create!(grade: grade, full_name: "Pedter Donal", absent_days: 5)
+
+    student_count = Student.get_students_count 
+    puts "COUNT, #{student_count}"
+    assert_equal 3, student_count
+  end
 end
